b5d17fcf1c0c9c1e367de82c168fc1f6cb59c319
trevor dewindt <trevor.dewindt@gmail.com>
2023-11-06 22:10:55 -0700

    final code

--- javascript/fibonacci_recursive.js
+++ javascript/fibonacci_recursive.js
@@ -1,5 +1,8 @@
-function fibonacci(n) {
-  // type your code here
+function fibonacci(n, arr = [0,1]) {
+  if (n < 2) return n
+  else if (arr.length - 1 === n) return arr[n]
+  arr.push(arr[arr.length - 1] + arr[arr.length - 2])
+  return fibonacci(n, arr)
 }
 
 if (require.main === module) {
@@ -22,3 +25,12 @@ module.exports = fibonacci;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+// Find the nth element in the Fibonacci series. 
+// The Fibonacci sequence starts with a 0 followed by a 1. 
+// After that, every value is the sum of the two values preceding it. 
+// Here are the first seven values as an example: 0, 1, 1, 2, 3, 5, 8.
+// Base Cases: if n < 2 return n; else if arr.length === n return arr.pop
+// Create an array: [0, 1] in params
+// add base cases
+// push array.length - 1 + array.length - 2
+// call function
